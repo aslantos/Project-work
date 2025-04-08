4082516c7e62a1d4f3f7e895e80d3a715e3c91af
aslantos <aslantb007@gmail.com>
2025-04-09 01:43:04 +0500

    create: gitignore

--- app/urls.py
+++ app/urls.py
@@ -21,6 +21,6 @@ from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('', views.index, name = 'index'),
+    path('about/', views.about, name = 'about'),
 ]

c0eaaee13b4d9a69ded6f0bce119315b94dd80e1
aslantos <aslantb007@gmail.com>
2025-04-08 23:14:44 +0500

    check up

--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ def index(request):
     context = {
         'title': 'Home',
         'content': 'Главная страница магазина - HOME',
+            
     }
 
     return render(request, 'main/index.html', context)
